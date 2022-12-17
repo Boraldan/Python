7735b11d027e9493f855720fb71340664da3d2a6
Alex Borodulin <boraldan@gmail.com>
2022-12-17 21:57:24 +0300

    DZ2 _ 5

--- DZ2/5.py
+++ DZ2/5.py
@@ -1,1 +1,19 @@
 # Реализуйте алгоритм перемешивания списка
+
+import random
+
+N = int(input('введите размер списка = '))
+
+list = list(range(0,N))
+
+for el in range(0,N):
+    i = random.randint(0, N-1)
+    temp1 = list[i] 
+    j = random.randint(0, N-1)
+    temp2 = list[j]
+    list[i] = temp2
+    list[j] = temp1
+
+print(f"{list}")
+
+ 
