4162830773d22e4df7860e5fa7e55ba4b78edc97
Alex Borodulin <boraldan@gmail.com>
2022-12-21 20:49:45 +0300

    DZ3 _ 4

--- DZ3/4.py
+++ DZ3/4.py
@@ -3,3 +3,17 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+
+d = int(input('введите число = '))
+
+bi_L=[]
+
+while d > 0:
+    if  d%2 ==0:
+        d = d//2
+        bi_L.append(0)
+    else:
+        d = (d-1)//2
+        bi_L.append(1)
+
+print(list(reversed(bi_L)))
