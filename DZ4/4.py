32ee962f75785e817c98bce6743adaa11224b8a9
Alex Borodulin <boraldan@gmail.com>
2022-12-26 16:01:35 +0300

    DZ4 _ 4

--- DZ4/4.py
+++ DZ4/4.py
@@ -4,3 +4,37 @@
 #     Пример:
 
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+import random
+
+def K_str(k):
+    mn = ""
+    for el in range(k, 0, -1):
+        A = random.randint(0,100)
+        if A != 0:
+            if el ==1:
+                mn = mn + " + " + str(A)+ "*x"
+            elif el == k:
+                mn = mn +str(A)+ "*x**"+str(el)
+            else:
+                mn = mn + " + " + str(A)+ "*x**"+str(el)
+    A = random.randint(0,100)
+    if A != 0:
+        mn = mn + " + " + str(A) + " = 0"
+    else:
+        mn = mn  + " = 0"
+    return mn
+
+def w_file(in_str):
+    with open('DZ4\\file.txt', 'w') as data:   # закроет соединение автоматически с файлом
+        data.write(in_str)
+
+def main():
+    num = int(input('Введите степень К =  '))
+    my_K = K_str(num)
+    print(my_K)
+    w_file(my_K)
+
+
+if __name__ == "__main__":
+	main()
