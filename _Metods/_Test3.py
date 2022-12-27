52ce10ae23a40d806bae046d7d6013704f5f331e
Alex Borodulin <boraldan@gmail.com>
2022-12-27 21:54:43 +0300

    DZ4 _ 5.1

---
+++ _Metods/_Test3.py
@@ -0,0 +1,3 @@
+myL1 = ("3*x + 5 = 0").split()
+
+print(myL1[2].isdigit())
--- _Metods/_Test_2.py
+++ _Metods/_Test_2.py
@@ -1,16 +1,56 @@
-in_L = [10, 3, 3, 3, 1, 4, 3, 3, 10, 3, 7, 3, 5, 5]
 
-temp = []
+path = "DZ4\\task_5\\file1.txt"
+data = open(path, 'r')
+for line in data:
+    myL1 =  line
+data.close()
 
-i = 0
-while i < len(in_L)-1:
-    if in_L[i] in in_L[i+1:]:
-        temp.append(in_L[i])
-    i=i+1
+path = "DZ4\\task_5\\file2.txt"
+data = open(path, 'r')
+for line in data:
+    myL2 =  line
+data.close()
 
-print(in_L)
 
-Fin = list(set(in_L) - set(temp))
+myL1 = myL1.split()
+myL2 = myL2.split()
 
-print(Fin)
+if 'x**' in myL1[0]:
+    N = myL1[0].find('x') + 2
+    t = myL1[0]       # выделяем отдельную переменную из списка
+    n = int(t[N+1:])  # степень 
+    A = int(t[:N-3])  # коофициент 
 
+else:
+    n = 1
+    print(n)
+
+while n > 1:
+    for i1, el1 in enumerate(myL1):
+        for i2, el2 in enumerate(myL2):
+            if 'x**' + str(n) in el1 and 'x**' + str(n) in el2:
+                    N1 = el1.find('x')
+                    A1 = int(el1[:N1-1])  # коофициент 
+                    N2 = el2.find('x')
+                    A2 = int(el2[:N2-1])  # коофициент 
+                    temp = str(A1+A2) + '*x**' + str(n)
+                    n = n - 1
+                    myL1[i1] = temp
+                    print(temp)
+            elif el1[-1:] == 'x' and el2[-1:] == 'x':
+                    N1 = el1.find('x')
+                    A1 = int(el1[:N1-1])  # коофициент 
+                    N2 = el2.find('x')
+                    A2 = int(el2[:N2-1])  # коофициент 
+                    temp = str(A1+A2) + '*x'
+                    myL1[i1] = temp
+                    print(temp)
+            elif  (el1.isdigit() and int(el1) > 0) and (el2.isdigit() and int(el2) > 0) :
+                    temp = int(el1) + int(el2)
+                    myL1[i1] = str(temp)
+                    print(temp)
+
+print(myL1)
+
+with open('DZ4\\task_5\\task5_file.txt', 'w') as data: 
+    data.write(str(myL1))
