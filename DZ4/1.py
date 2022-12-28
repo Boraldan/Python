fb98c95e25a958a37ae67e8cd0dd93d53c727e1f
Alex Borodulin <boraldan@gmail.com>
2022-12-28 11:47:02 +0300

    DZ4 _ 1.1

--- DZ4/1.py
+++ DZ4/1.py
@@ -3,15 +3,20 @@
 #  при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
 
 
+
+from math import pi 
+
+
+
 def RoundNum(num1, d1):
     num1 = round(num1, d1)
     return num1
 
 
 def main():
-    num = float(input('введите число = '))
+    # num = float(input('введите число = '))
     d = int(input('введите количество знаков после запятой от 1 до 10 = '))
-    print(RoundNum(num, d))
+    print(RoundNum(pi, d))
 
 if __name__ == "__main__":
 	main()
--- _Metods/_TEST_1.py
+++ _Metods/_TEST_1.py
@@ -1,76 +1,17 @@
-path = "DZ4\\task_5\\file1.txt"
-data = open(path, 'r')
-for line in data:
-    max =  line
-data.close()
 
-path = "DZ4\\task_5\\file2.txt"
-data = open(path, 'r')
-for line in data:
-    min =  line
-data.close()
-
-print('Первый многочлен : ' + max)
-print('Второй многочлен : ' + min)
-
-max = max.split()
-min = min.split()
-
-if len(max) > len(min):
-    max = max
-    min = min
-else:
-    max = min
-    min = max
-
-
-if 'x**' in max[0]:
-    N = max[0].find('x') + 2
-    t = max[0]       # выделяем отдельную переменную из списка
-    n = int(t[N+1:])  # степень 
-    # A = int(t[:N-3])  # коофициент 
-
-else:
-    n = 1
-    print(n)
-
-count1 = 0
-count2 = 0
-
-while n > 0:
-    for i1, el1 in enumerate(max):
-        for i2, el2 in enumerate(min):
-            if 'x**' + str(n) in el1 and 'x**' + str(n) in el2:
-                    N1 = el1.find('x')
-                    A1 = int(el1[:N1-1])  # коофициент 
-                    N2 = el2.find('x')
-                    A2 = int(el2[:N2-1]) 
-                    max[i1] = str(A1+A2) + '*x**' + str(n)
-            elif el1[-1:] == 'x' and el2[-1:] == 'x':
-                    N1 = el1.find('x')
-                    A1 = int(el1[:N1-1])  
-                    N2 = el2.find('x')
-                    A2 = int(el2[:N2-1])   
-                    temp = str(A1+A2) + '*x'
-                    if count1 == 0:
-                        max[i1] = temp
-                        count1 +=1
-            elif  (el1.isdigit() and int(el1) > 0) and (el2.isdigit() and int(el2) > 0) :
-                    temp = int(el1) + int(el2)
-                    if count2 == 0:
-                        max[i1] = str(temp)
-                        count2 +=1
-    n = n - 1
+from math import pi 
 
 
 
-newL = ''
+def RoundNum(num1, d1):
+    num1 = round(num1, d1)
+    return num1
 
-for el in max:
-    newL += str(el) + ' '
 
-print('Итоговый многочлен: ' + newL)
+def main():
+    # num = float(input('введите число = '))
+    d = int(input('введите количество знаков после запятой от 1 до 10 = '))
+    print(RoundNum(pi, d))
 
-with open('DZ4\\task_5\\task5_file.txt', 'w') as data: 
-    data.write(newL)
-
+if __name__ == "__main__":
+	main()
